b58a684ee5449a6bc0225d05dfb3ad7de28c3b7c
bangdit28 <adityahikmahtiar866@gmail.com>
2025-10-28 03:07:10 +0700

    Update database.js

--- assets/data/database.js
+++ assets/data/database.js
@@ -4,34 +4,24 @@ const initialMovieData = [
         title: "Godzilla x Kong: The New Empire",
         description: "Dua raksasa kuno, Godzilla dan Kong, berbenturan dalam pertempuran spektakuler seiring umat manusia mengungkap asal usul mereka yang saling terkait.",
         posterUrl: "https://image.tmdb.org/t/p/w500/AtsgWhDnHTq68L0lLsUrCnM7TjG.jpg",
-        category: "populer"
+        category: "populer",
+        trailerUrl: "https://www.youtube.com/embed/qqrpMRDuPfc" // <-- TAMBAHKAN INI
     },
     {
         id: 2,
         title: "Dune: Part Two",
         description: "Paul Atreides bersatu dengan Chani dan Fremen saat membalas dendam terhadap para konspirator yang menghancurkan keluarganya.",
         posterUrl: "https://image.tmdb.org/t/p/w500/1pdfLvkbY9ohJlCjQH2CZjjYVvJ.jpg",
-        category: "populer"
-    },
-    {
-        id: 3,
-        title: "The Batman",
-        description: "Ketika seorang pembunuh berantai sadis mulai membunuh tokoh-tokoh politik penting di Gotham, Batman terpaksa menyelidiki korupsi tersembunyi di kota itu.",
-        posterUrl: "https://image.tmdb.org/t/p/w500/d5NXSklXo0qyIY2VFIh5dcfKEjo.jpg",
-        category: "baru"
+        category: "populer",
+        trailerUrl: "https://www.youtube.com/embed/U2Qp5pL3ovA" // <-- TAMBAHKAN INI
     },
     {
         id: 4,
         title: "Agak Laen",
         description: "Empat sekawan yang mengelola rumah hantu di pasar malam, mencari cara baru untuk menakuti pengunjung agar tidak bangkrut.",
         posterUrl: "https://image.tmdb.org/t/p/w500/v392z3eodG2mHaf5Cs23m9a3k3.jpg",
-        category: "indonesia"
-    },
-    {
-        id: 5,
-        title: "Poor Things",
-        description: "Kisah fantastik tentang evolusi Bella Baxter, seorang wanita muda yang dihidupkan kembali oleh seorang ilmuwan yang brilian dan tidak ortodoks.",
-        posterUrl: "https://image.tmdb.org/t/p/w500/kDp1vUBnMpe8ak4rjgl3cLELqjU.jpg",
-        category: "baru"
+        category: "indonesia",
+        trailerUrl: "https://www.youtube.com/embed/HqZp-y_8-sA" // <-- TAMBAHKAN INI
     }
+    // ...tambahkan ke film lainnya juga
 ];
